a4a725b35a84f510b88e42786af5a68ab5f07d16
AStepaniuk <AlexandrStepaniuk@gmail.com>
2012-11-18 13:36:48 +0300

    Sequential cases generation functionality is added.

--- qunit-parameterize.js
+++ qunit-parameterize.js
@@ -5,6 +5,8 @@
  */
 QUnit.extend(QUnit, {
 	cases : function(testCases) {
+		var currentCases = testCases;
+
 		var createTest = function(methodName, title, expected, callback, parameters) {
 			QUnit[methodName](
 				title,
@@ -14,15 +16,15 @@ QUnit.extend(QUnit, {
 		};
 
 		var iterateTestCases = function(methodName, title, expected, callback) {
-			if (!testCases) return;
+			if (!currentCases) return;
 
 			if (!callback) {
 				callback = expected;
 				expected = null;
 			}
 
-			for (var i = 0; i < testCases.length; ++i) {
-				var parameters = testCases[i];
+			for (var i = 0; i < currentCases.length; ++i) {
+				var parameters = currentCases[i];
 
 				var testCaseTitle = title;
 				if (parameters.title) {
@@ -33,7 +35,62 @@ QUnit.extend(QUnit, {
 			}
 		}
 
+		var getLength = function(arr) {
+			return arr ? arr.length : 0;
+		}
+
+		var getItem = function(arr, idx) {
+			return arr ? arr[idx] : undefined;
+		}
+		
+		var mix = function(testCase, mixData) {
+			if (testCase && mixData) {
+				var result = clone(testCase);
+				for(var p in mixData) {
+					if (p !== "title") {
+						if (!(p in result))  result[p] = mixData[p];
+					} else {
+						result[p] = [result[p], mixData[p]].join("");
+					}
+				}
+				return result;
+			} else if (testCase) {
+				return testCase;
+			} else if (mixData) {
+				return mixData;
+			} else {
+				// return null or undefined whatever testCase is
+				return testCase;
+			}
+		}
+
+		var clone = function(testCase) {
+			var result = {};
+			for (var p in testCase) {
+				result[p] = testCase[p];
+			}
+			return result;
+		}
+
 		return {
+			sequential : function(addData) {
+				var casesLength = getLength(currentCases);
+				var addDataLength = getLength(addData);
+				var length = casesLength > addDataLength ? casesLength : addDataLength;
+
+				var newCases = [];
+				for (var i = 0; i < length; ++i) {
+					var currentCaseI = getItem(currentCases, i);
+					var dataI = getItem(addData, i);
+					var newCase = mix(currentCaseI, dataI);
+
+					if (newCase) newCases.push(newCase);
+				}
+				currentCases = newCases;
+
+				return this;
+			},
+
 			test : function(title, expected, callback) {
 				iterateTestCases("test", title, expected, callback);
 			},
---
+++ test/sequential-cases-generation-test.js
@@ -0,0 +1,205 @@
+(function(){
+	QUnit.module("Sequential tests generation");
+	
+	var sequentialTestCases = [
+		{
+			title : "1x1, properties addition",
+			originalCases : [{ a : 1 }],
+			addData : [{ b : "val_b" }],
+			expectedCases : [{ a : 1, b : "val_b" }]
+		},
+		{
+			title : "3x3, properties addition",
+			originalCases : [{ a : 1 }, { a : 2 }, { a : 3 }],
+			addData : [{ b : "v1" }, { b : "v2" }, { b : "v3" }],
+			expectedCases : [{ a : 1, b : "v1" }, { a : 2, b : "v2" }, { a : 3, b : "v3" }]
+		},
+		{
+			title : "1x3, properties addition",
+			originalCases : [{ a : 1 }],
+			addData : [{ b : "v1" }, { b : "v2" }, { b : "v3" }],
+			expectedCases : [{ a : 1, b : "v1" }, { b : "v2" }, { b : "v3" }]
+		},
+		{
+			title : "3x1, properties addition",
+			originalCases : [{ a : 1 }, { a : 2 }, { a : 3 }],
+			addData : [{ b : "v1" }],
+			expectedCases : [{ a : 1, b : "v1" }, { a : 2 }, { a : 3 }]
+		},
+		{
+			title : "0x3, properties addition",
+			originalCases : [],
+			addData : [{ b : "v1" }, { b : "v2" }, { b : "v3" }],
+			expectedCases : [{ b : "v1" }, { b : "v2" }, { b : "v3" }]
+		},
+		{
+			title : "3x0, properties addition",
+			originalCases : [{ a : 1 }, { a : 2 }, { a : 3 }],
+			addData : [],
+			expectedCases : [{ a : 1 }, { a : 2 }, { a : 3 }]
+		},
+		{
+			title : "undefined-cases considering",
+			originalCases : [{ a : 1 }, undefined, { a : 3 }],
+			addData : [undefined, { b : "v2" }, { b : "v3" }],
+			expectedCases : [{ a : 1 }, { b : "v2" }, { a : 3, b : "v3" }]
+		},
+		{
+			title : "null-cases considering",
+			originalCases : [null, { a : 2 }, { a : 3 }],
+			addData : [{ b : "v1" }, null, { b : "v3" }],
+			expectedCases : [{ b : "v1" }, { a : 2 }, { a : 3, b : "v3" }]
+		},
+		{
+			title : "undefined x 1",
+			originalCases : undefined,
+			addData : [{ b : "v1" }],
+			expectedCases : [{ b : "v1" }]
+		},
+		{
+			title : "null x 1",
+			originalCases : null,
+			addData : [{ b : "v1" }],
+			expectedCases : [{ b : "v1" }]
+		},
+		{
+			title : "1 x undefined",
+			originalCases : [{ a : 1 }],
+			addData : undefined,
+			expectedCases : [{ a : 1 }]
+		},
+		{
+			title : "1 x null",
+			originalCases : [{ a : 1 }],
+			addData : null,
+			expectedCases : [{ a : 1 }]
+		},
+		{
+			title : "undefined x undefined",
+			originalCases : undefined,
+			addData : undefined,
+			expectedCases : []
+		},
+		{
+			title : "null x null",
+			originalCases : null,
+			addData : null,
+			expectedCases : []
+		},
+		{
+			title : "[{}, undefined] x undefined",
+			originalCases : [{ a : 1 }, undefined],
+			addData : undefined,
+			expectedCases : [{ a : 1 }]
+		},
+		{
+			title : "undefined x [undefined, {}]",
+			originalCases : undefined,
+			addData : [undefined, { b : "v2" }],
+			expectedCases : [{ b : "v2" }]
+		},
+		{
+			title : "[null, {}] x null",
+			originalCases : [null, { a : 2 }],
+			addData : null,
+			expectedCases : [{ a : 2 }]
+		},
+		{
+			title : "null x [{}, null]",
+			originalCases : null,
+			addData : [{ b : "v1" }, null],
+			expectedCases : [{ b : "v1" }]
+		},
+		{
+			title : "Param name conflicts",
+			originalCases : [{ a : 1 }, { a : 2 }, { a : undefined }, { a : null }, {}, {}, {}, {} ],
+			addData : [{ a : 10 }, {  }, { a : 30  }, { a : 40 } , { a : 50 }, { a : undefined }, { a : null }, {}],
+			expectedCases : [{ a : 1 }, { a : 2 }, { a : undefined }, { a : null }, { a : 50 }, { a : undefined }, { a : null }, {}]
+		},
+		{
+			title : "title concatenation",
+			originalCases : [{ title : "t1", a : 1 }, { title : "t2", a : 2 }, { a : 3 }],
+			addData : [{ title : "d1", b : "v1" }, { b : "v2" }, { title : "d3", b : "v3" }],
+			expectedCases : [{ title : "t1d1", a : 1, b : "v1" }, { title : "t2", a : 2, b : "v2" }, {title : "d3",  a : 3, b : "v3" }]
+		}
+	];
+
+	var registerTest = function(testCase) {
+		QUnit.test(testCase.title, function(){
+			var actualTestCases = [];
+
+			var record = recordCalls(QUnit, "test", function() {
+				QUnit
+					.cases(testCase.originalCases)
+					.sequential(testCase.addData)
+					.test("", function(actualTestCase){
+						// Test callback saves passed actualTestCase object.
+						actualTestCases.push(actualTestCase)
+					});
+			});
+
+			var recordedArguments = record.getArguments();
+			for(var i = 0; i < recordedArguments.length; ++i) {
+				// execute test callback which is passed as 3-rd argument
+				recordedArguments[i][2]();
+			}
+			assert.deepEqual(actualTestCases, testCase.expectedCases);
+		});
+	}
+	
+	for (var i = 0; i < sequentialTestCases.length; ++i) {
+		var testCase = sequentialTestCases[i];
+		registerTest(testCase);
+	}
+
+
+	QUnit.test("Don not change original test cases objects", function(){
+		var originalCases = [{ a : 1 }, { a : 2 }];
+		var addData = [{ b : "v1" }, { b : "v2" }];
+		var actualTestCases = [];
+
+		var record = recordCalls(QUnit, "test", function() {
+			QUnit
+				.cases(originalCases)
+				.sequential(addData)
+				.test("", function(actualTestCase){});
+		});
+
+		assert.deepEqual(originalCases, [{ a : 1 }, { a : 2 }]);
+	});
+
+
+	QUnit.test("Fluent calls support", function(){
+		var originalCases = [{ a : 1 }, { a : 2 }];
+		var addData1 = [{ b : "v1" }, { b : "v2" }];
+		var addData2 = [{ c : ["arr1"] }, { c : ["arr2"] }];
+		var addData3 = [{ d : { p : "p1"} }, { d : { p : "p2" } }];
+		var actualTestCases = [];
+
+		var record = recordCalls(QUnit, "test", function() {
+			QUnit
+				.cases(originalCases)
+				.sequential(addData1)
+				.sequential(addData2)
+				.sequential(addData3)
+				.test("", function(actualTestCase){
+					// Test callback saves passed actualTestCase object.
+					actualTestCases.push(actualTestCase)
+				});
+		});
+
+		var recordedArguments = record.getArguments();
+		for(var i = 0; i < recordedArguments.length; ++i) {
+			// execute test callback which is passed as 3-rd argument
+			recordedArguments[i][2]();
+		}
+
+		assert.deepEqual(
+			actualTestCases,
+			[
+				{ a : 1, b : "v1", c : ["arr1"], d : { p : "p1"} },
+				{ a : 2, b : "v2", c : ["arr2"], d : { p : "p2"} }
+			]
+		);
+	});
+})();
