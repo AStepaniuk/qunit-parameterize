5c5db731749ef675148d585d7add0f2284da18d4
Marek Jędryka <marek629@poczta.onet.pl>
2016-11-03 23:32:56 +0100

    Cleaned test code.

--- test/skip-case-test.js
+++ test/skip-case-test.js
@@ -1,30 +1,24 @@
-//var skipCaseArray = [
-//    {
-//        title: 'skip default value',
-//        cases: [
-//            {
-//                title: 'skip default is false',
-//                expected: false
-//            },
-//            {
-//                title: 'skip is false',
-//                skip: false,
-//                expected: false
-//            },
-//            {
-//                title: 'skip is true',
-//                skip: true,
-//                expected: true
-//            },
-//        ]
-//    }
-//];
-
-//var QUnitMock = QUnit;
-//QUnitMock.skip = function(message)
-//{
-//    QUnitMock.skipMessage = message;
-//};
+var skipCaseArray = [
+   {
+       title: 'skip default value',
+       cases: [
+           {
+               title: 'skip default is false',
+               expected: false
+           },
+           {
+               title: 'skip is false',
+               skip: false,
+               expected: false
+           },
+           {
+               title: 'skip is true',
+               skip: true,
+               expected: true
+           },
+       ]
+   }
+];
 
 QUnit.module('Skip case');
 QUnit.skip('Check if QUnit.skip() function is available.');
@@ -46,31 +40,5 @@ QUnit.cases([
 ]).test('skip default value',
 function(params, assert)
 {
-    console.log(params);
-    assert.ok(!params.expected);
+    assert.notOk(params.expected);
 });
-
-
-//parameterizeFixture.registerModule(
-//    QUnit,
-//    'Skip case test',
-//    skipCaseArray,
-//    function(qunit, assert, testName, testCases)
-//    {
-//        console.log(testName, testCases);
-//        var title = "some test title";
-//		var record = recordCalls(qunit, testName, function()
-//        {
-//			qunit
-//				.cases(testCases)
-//				[testName](title, function(testCase){console.log(testCase);});
-//		});
-//
-//		var recordedArguments = record.getArguments();
-//		for (var i = 0; i < recordedArguments.length; ++i) {
-//			//var isSkiped = record.neverCalled();
-//			//assert.equal(recordedArguments[i][0], title);
-//            console.log(recordedArguments[i]);
-//		}
-//    }
-//);
