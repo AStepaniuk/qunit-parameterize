7561edcaedc629363f653057db247e1c7321e4ba
Marek Jędryka <marek629@poczta.onet.pl>
2016-12-28 22:47:14 +0100

    Removed unused code from skip case test.

--- test/skip-case-test.js
+++ test/skip-case-test.js
@@ -1,25 +1,3 @@
-var skipCaseArray = [
-   {
-       title: 'skip default value',
-       cases: [
-           {
-               title: 'skip default is false',
-               expected: false
-           },
-           {
-               title: 'skip is false',
-               skip: false,
-               expected: false
-           },
-           {
-               title: 'skip is true',
-               skip: true,
-               expected: true
-           },
-       ]
-   }
-];
-
 QUnit.module('Skip case');
 QUnit.skip('Check if QUnit.skip() function is available.');
 QUnit.cases([
